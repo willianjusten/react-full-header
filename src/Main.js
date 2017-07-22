7983ce549fc25dc67ad69c53d0a668618e9ef874
Willian Justen <willianjustenqui@gmail.com>
2017-07-22 20:52:37 -0300

    Add Title prop

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const FullHeader = () => (
-    <header></header>
-);
+const FullHeader = ({ title }) => {
+    const component = (
+        <header>
+            {title && <h1>{title}</h1>}
+        </header>
+    );
+
+    return component;
+};
+
+const propTypes = {
+    title: PropTypes.string,
+};
+
+FullHeader.propTypes = propTypes;
 
 export default FullHeader;
--- tests/specs/Main.spec.js
+++ tests/specs/Main.spec.js
@@ -8,4 +8,19 @@ describe('<FullHeader />', () => {
         const wrapper = shallow(<FullHeader />);
         expect(wrapper.find('header')).to.have.length(1);
     });
+
+    it('should have h1 tag when title passed', () => {
+        const wrapper = shallow(<FullHeader title="TDD" />);
+        expect(wrapper.find('h1')).to.have.length(1);
+    });
+
+    it('should not have h1 tag when title is not passed', () => {
+        const wrapper = shallow(<FullHeader />);
+        expect(wrapper.find('h1')).to.have.length(0);
+    });
+
+    it('should have h1 tag with the title passed', () => {
+        const wrapper = shallow(<FullHeader title="TDD" />);
+        expect(wrapper.find('h1').props().children).to.be.equal("TDD");
+    });
 });
