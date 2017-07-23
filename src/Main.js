4c29e3669f9ccf4c60f1a74d88e4cf5e4ed45eca
Willian Justen <willianjustenqui@gmail.com>
2017-07-23 19:36:44 -0300

    Add Styles

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { headerStyle, containerStyle, titleStyle, subtitleStyle } from './Styles';
 
 const defaultProps = {
     bgColor: '#ccc',
@@ -18,7 +19,8 @@ const propTypes = {
 };
 
 const FullHeader = ({ title, subtitle, bgColor, textColor, font, bgImg }) => {
-    const headerStyles = {
+    const headerStyleCombined = {
+        ...headerStyle,
         backgroundColor: bgColor,
         backgroundImage: `url(${bgImg})`,
         color: textColor,
@@ -26,9 +28,11 @@ const FullHeader = ({ title, subtitle, bgColor, textColor, font, bgImg }) => {
     };
 
     const component = (
-        <header style={headerStyles}>
-            {title && <h1>{title}</h1>}
-            {subtitle && <h2>{subtitle}</h2>}
+        <header style={headerStyleCombined}>
+            <div style={containerStyle}>
+                {title && <h1 style={titleStyle}>{title}</h1>}
+                {subtitle && <h2 style={subtitleStyle}>{subtitle}</h2>}
+            </div>
         </header>
     );
 
---
+++ src/Styles.js
@@ -0,0 +1,24 @@
+export const titleStyle = {
+    fontSize: '5rem',
+    lineHeight: '1.5',
+};
+
+export const subtitleStyle = {
+    fontSize: '2rem',
+    lineHeight: '1.5',
+};
+
+export const headerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    textAlign: 'center',
+    width: '100%',
+    height: '100vh',
+    backgroundSize: 'cover',
+};
+
+export const containerStyle = {
+    width: '100%',
+    position: 'relative',
+    zIndex: 2,
+};
